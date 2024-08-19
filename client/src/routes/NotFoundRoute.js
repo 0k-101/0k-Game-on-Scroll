bdae79135762c957ca6cb096a5294aafe1f68db2
Alperen Tekin <121463341+sirALp@users.noreply.github.com>
2024-08-20 01:33:59 +0300

    not found page done

--- client/src/routes/NotFoundRoute.js
+++ client/src/routes/NotFoundRoute.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
 export default function NotFound() {
+    const catImg = require('../assets/angry-cat.png')
     return (
-        <div className='d-flex flex-column gap-4 w-25 m-auto' style={{height:'100vh'}}> 
-            <h2 className='mt-5'>
-                404 Not Found
-            </h2>
-            <NavLink className='btn btn-primary my-auto' 
+        <div className='not-found-container d-flex flex-column gap-4 w-25 m-auto' style={{height:'100vh'}}> 
+            <img src={catImg} alt="Angry Cat" className='m-auto' />
+            <h2 className='cat-speek'>  <strong style={{fontSize:'4rem'}}>404 NOT FOUND!</strong> <br/> 
+            GO BACK WHERE YOU CAME FROM!!</h2>
+            <NavLink className='btn btn-secondary go-back-btn my-auto' 
                 to={'/'}
                 >Go back</NavLink>
         </div>
