a676cf52a9982fbe85e1aa24d75b2bbb9805ec39
Bora ILCI <borailci@outlook.com>
2024-08-22 16:02:44 +0300

    perFinder revise

--- client/src/components/PerFinder.js
+++ client/src/components/PerFinder.js
@@ -1,5 +1,6 @@
 import { Button } from 'react-bootstrap'
-
+import Per from '../constants/Per'
+// sahte okey ile renkli per 
 const spliceHand = (hand, okeyId) => {
     let cleanHands = []
     let cleanHand = []
@@ -27,21 +28,27 @@ const spliceHand = (hand, okeyId) => {
 }
 
 const checkForSequence = (hand, okeyId) => {
+    let hasOkey = false
+    let okeyValue = 0
+    let points = 0
     for (let i = 0; i < hand.length - 1; i++) {
         if (hand[i] === 53 || hand[i] === 106) {
             hand[i] = okeyId
         }
         if (hand[i + 1] === 53 || hand[i + 1] === 106) {
             hand[i + 1] = okeyId
-
         }
         let leftcard = hand[i] % 53
         let rightcard = hand[i + 1] % 53
         if (leftcard === okeyId) {
             leftcard = ((hand[i + 1]) % 53) - 1
+            hasOkey = true
+            okeyValue = Math.trunc(leftcard / 13)
         }
         if (rightcard === okeyId) {
             rightcard = hand[i] + 1 % 53
+            hasOkey = true
+            okeyValue = Math.trunc(rightcard / 13)
         }
         if (Math.trunc(leftcard / 13) === Math.trunc(rightcard / 13)) {
             if ((leftcard % 13) + 1 !== rightcard % 13) {
@@ -56,21 +63,78 @@ const checkForSequence = (hand, okeyId) => {
         }
         else return false
     }
-    return true
+    if (hasOkey) {
+        for (let i = 0; i < hand.length; i++) {
+            if (!hand[i] === okeyId) {
+                let cardValue = hand[i] % 13
+                if (cardValue === 0) {
+                    cardValue == 13
+                }
+            }
+            else if (i === 0) {
+                let cardValue = hand[i + 1] % 13
+                if (cardValue === 0) {
+                    cardValue == 13
+                    cardValue = cardValue - 1
+                }
+            }
+            points += cardValue
+        }
+    }
+    else {
+        for (let i = 0; i < hand.length; i++) {
+            let cardValue = hand[i] % 13
+            if (cardValue === 0) {
+                cardValue == 13
+            }
+            points += cardValue
+        }
+    }
+    return new Per(false, hasOkey, okeyValue, points, hand)
 }
 
 
 const checkForSameColor = (hand, okeyId) => {
+    let hasOkey = false
+    let okeyValue = 0
+    let points = 0
+    let cardValue = 0
+    const cardColors = ['gray', 'blue', 'pink', 'orange']
+    if (hand.length > 5 || hand.length < 3) {
+        return false
+    }
     let uniqueColors = []
-    let cardValue = hand[0] % 13
+    if (hand[0] === okeyId && hand[1] === okeyId) {
+        cardValue = hand[2] % 13
+    }
+    else if (hand[0] === okeyId && hand[1] !== okeyId) {
+        cardValue = hand[1] % 13
+    }
+    else if (hand[0] !== okeyId && hand[1] !== okeyId) {
+        cardValue = hand[0] % 13
+    }
     for (let i = 0; i < hand.length - 1; i++) {
         if (hand[i] === 53 || hand[i] === 106) {
-            hand[i] = okeyId
+            let cardColor = cardColors(Math.trunc(okeyId / 13))
+            let _cardValue = okeyId % 13
+            if (cardValue != _cardValue) return false
+            if (uniqueColors.includes(cardColor)) {
+                return false
+            }
+            else uniqueColors.push(cardColor)
         }
-        if (hand[i + 1] === 53 || hand[i + 1] === 106) {
-            hand[i + 1] = okeyId
+        else if (hand[i] === okeyId) {
+            if (i === 0) {
+                cardValue = hand[i + 1] % 13
+                okeyValue = cardValue
+            }
+            else {
+                cardValue = hand[i - 1] % 13
+                okeyValue = cardValue
+            }
+            hasOkey = true
         }
-        let cardColor = Math.trunc(hand[i] / 13)
+        let cardColor = cardColors(Math.trunc(hand[i] / 13))
         let _cardValue = hand[i] % 13
         if (cardValue != _cardValue) return false
         if (uniqueColors.includes(cardColor)) {
@@ -78,23 +142,8 @@ const checkForSameColor = (hand, okeyId) => {
         }
         else uniqueColors.push(cardColor)
     }
-    return true
-}
-
-const pointCalculator = (pers) => {
-    let points = 0
-    pers.forEach(hand => {
-        hand.forEach(card => {
-            if (cardId === okeyId) {
-
-            }
-            let cardValue = card % 13
-            if (cardValue === 0) cardValue = 13
-            points += cardValue
-        })
-    }
-    )
-    return points
+    points = (cardValue === 0 ? 13 : cardValue) * hand.length
+    return new Per(true, hasOkey, okeyValue, points, hand)
 }
 
 const perFinder = (hand, okeyId) => {
@@ -110,17 +159,30 @@ const perFinder = (hand, okeyId) => {
         return []
     }
     upper.forEach(hand => {
-        if (checkForSequence(hand, okeyId) || checkForSameColor(hand, okeyId)) {
-            pers.push(hand)
+        let possiblePer = checkForSequence(hand, okeyId)
+        if (possiblePer) {
+            pers.push(possiblePer)
+        }
+        else {
+            possiblePer = checkForSameColor(hand, okeyId)
+            if (possiblePer) {
+                pers.push(possiblePer)
+            }
         }
     })
     lower.forEach(hand => {
-        if (checkForSequence(hand, okeyId) || checkForSameColor(hand, okeyId)) {
-            pers.push(hand)
+        let possiblePer = checkForSequence(hand, okeyId)
+        if (possiblePer) {
+            pers.push(possiblePer)
+        }
+        else {
+            possiblePer = checkForSameColor(hand, okeyId)
+            if (possiblePer) {
+                pers.push(possiblePer)
+            }
         }
     })
-    points = pointCalculator(pers)
-    return ({ pers, points })
+    return ({ pers })
 }
 export default function PerFinder({ hand }) {
     const { pers, points } = perFinder([...hand]);
