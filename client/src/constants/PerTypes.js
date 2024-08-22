b0927ce3e81e89a0b9067400f4126e60a4a34658
Alperen Tekin <121463341+sirALp@users.noreply.github.com>
2024-08-22 15:24:51 +0300

    adjusted constant class Per

--- client/src/constants/PerTypes.js
+++ client/src/constants/PerTypes.js
@@ -1,9 +1,9 @@
-const PerTypes = {
-    isColorPer: false,
-    hasOkey: false,
-    okeysValue: null,
-    perTotalValue: 0,
-    perCardIds: [],
+class Per {
+    constructor (isColorPer,hasOkey,okeysValue,perTotalValue,perCardIds) {
+        this.isColorPer = isColorPer;
+        this.hasOkey = hasOkey;
+        this.okeysValue = okeysValue;
+        this.perTotalValue = perTotalValue;
+        this.perCardIds = perCardIds;
+    }
 }
-
-export default PerTypes;
